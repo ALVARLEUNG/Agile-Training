64e91672e53ea5a7b3f7eb4718f4c43de1c1bd21
ALVARLEUNG <alvar.leung@qq.com>
2019-08-10 15:42:29 +0800

    feat: add tests for GenerateReport function

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -1,14 +1,43 @@
 package sales;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
 
 public class SalesAppTest {
 
-	@Test
-	public void testGenerateReport() {
-		
-		SalesApp salesApp = new SalesApp();
-		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
-	}
+    @Test
+    public void testGenerateReport_giveSaleNotFound_thenReturnNull() {
+        SalesApp salesApp = spy(new SalesApp());
+        doReturn(null).when(salesApp).getSales(any());
+        salesApp.generateSalesActivityReport("DUMMY", false);
+        Mockito.verify(salesApp, times(1)).getSales(any());
+    }
+
+    @Test
+    public void testGenerateReport_giveSaleIdAndIsNatTrade() {
+        List<String> Headers = new ArrayList<>();
+        SalesApp salesApp = spy(new SalesApp());
+
+        doReturn(new Sales()).when(salesApp).getSales(any());
+        doReturn(new ArrayList<String>()).when(salesApp).getHeaders(anyBoolean());
+        doReturn(new ArrayList<SalesReportData>()).when(salesApp).getSalesReportDataList(any());
+        doReturn(new SalesActivityReport()).when(salesApp).generateReport(anyList(), anyList());
+        doNothing().when(salesApp).uploadReportDocument(any());
+
+        salesApp.generateSalesActivityReport("DUMMY", false);
+        Mockito.verify(salesApp, times(1)).getSales(any());
+        Mockito.verify(salesApp, times(1)).getSalesReportDataList(any());
+        Mockito.verify(salesApp, times(1)).generateReport(anyList(), anyList());
+        Mockito.verify(salesApp, times(1)).getHeaders(anyBoolean());
+        Mockito.verify(salesApp, times(1)).uploadReportDocument(any());
+    }
+
+
 }
